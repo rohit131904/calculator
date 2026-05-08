bb101bfb59d85d4672e5374dbf08567ce0a5c0a5
jivesh-xiarch <jivesh@xiarch.net>
2026-05-08 11:52:30 +0530

    Multiplication

--- index.js
+++ index.js
@@ -7,33 +7,60 @@ let operators = [];
 
 buttons.forEach(button => {
     button.addEventListener("click", () => {
+
         let value = button.dataset.value;
 
         if (!value) return;
 
+        // Numbers and decimal
         if (!isNaN(value) || value === ".") {
+
             handleNumber(value);
+
         }
-        else if(value === "%"){
+
+        // Operators
+        else if (value === "%" || value === "*") {
+
             handleOperator(value);
+
         }
+
+        // Equal button
         else if (value === "=") {
+
             calculateResult();
+
         }
+
+        // Clear button
         else if (value === "C") {
+
             clearAll();
+
         }
+
     });
 });
 
 function handleNumber(value) {
-    if (value === "." && currentInput.includes(".")) {  return;
+
+    // Prevent multiple decimals
+    if (value === "." && currentInput.includes(".")) {
+
+        return;
+
     }
+
     currentInput += value;
+
     updateDisplay();
+
 }
 
 function handleOperator(op) {
+
+    // Prevent operator without number
     if (currentInput === "") return;
 
     numbers.push(parseFloat(currentInput));
@@ -41,47 +68,71 @@ function handleOperator(op) {
     operators.push(op);
 
     currentInput = "";
+
     updateDisplay();
+
 }
 
 function calculateResult() {
+
     if (currentInput === "") return;
 
     numbers.push(parseFloat(currentInput));
 
+    // Process operators one by one
     for (let i = 0; i < operators.length; i++) {
 
-      if (operators[i] === "%") {
+        let first = numbers[i];
 
-    let first = numbers[i];
+        let second = numbers[i + 1];
 
-    let second = numbers[i + 1];
+        let result = 0;
 
-    let percentValue = (first / 100) * second;
+        // Percentage Logic
+        if (operators[i] === "%") {
 
-    numbers.splice(i, 2, percentValue);
+            result = (first / 100) * second;
 
-    operators.splice(i, 1);
+        }
 
-    i--;
+        // Multiplication Logic
+        else if (operators[i] === "*") {
 
-}
+            result = first * second;
+
+        }
+
+        // Replace old values with result
+        numbers.splice(i, 2, result);
+
+        operators.splice(i, 1);
+
+        i--;
+
     }
-    let result = numbers[0];
 
-    display.value = result;
+    let finalResult = numbers[0];
+
+    display.value = finalResult;
+
+    currentInput = finalResult.toString();
 
     numbers = [];
+
     operators = [];
-    currentInput = result.toString();
 
 }
 
 function clearAll() {
+
     currentInput = "";
+
     numbers = [];
+
     operators = [];
+
     display.value = "0";
+
 }
 
 function updateDisplay() {
